f15b0dfd9381ae87e79c215bb2bec1483df9df06
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2014-11-18 22:43:48 +0700

    the form going to a db, but not storing it yet

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'byebug'
+require "sqlite3"
+
+get '/all' do
+
+end
 
 get '/' do
   erb :index
+end
+
+post '/save' do
+  debugger
+  db = SQLite3::Database.new "photos.db"
+
+  erb :saved
 end
