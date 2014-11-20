980e655c2da8697c56542cc70e9c572449d59868
Tommy Sullivan and Vajrasky Kok <pair+tommy+vajrasky@example.com>
2014-11-20 22:55:26 +0700

    using images in base 64

--- app/uploaders/portrait_uploader.rb
+++ app/uploaders/portrait_uploader.rb
@@ -7,6 +7,8 @@ class PortraitUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  process :capture_base64
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -18,16 +20,19 @@ class PortraitUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fill => [64, 64]
-    process :capture_base64
   end
 
-  def thumb_base64
-    data = model.send "#{mounted_as}_thumb_base64"
+  def original_base64
+    data = model.send "#{mounted_as}_base64"
     "data:image/#{file.extension};base64,#{data}"
   end
 
-  def capture_base64
+  def capture_version_base64
     model.send "#{mounted_as}_#{version_name}_base64=", Base64.encode64(file.read)
+  end
+
+  def capture_base64
+    model.send "#{mounted_as}_base64=", Base64.encode64(file.read)
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
