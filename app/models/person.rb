ef2cd04762af8d7b09648252250102663bcafa20
Tommy Sullivan and Vajrasky Kok <pair+tommy+vajrasky@example.com>
2014-11-21 11:28:32 +0700

    create a thumbnail of the upload as well

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,17 @@
+# encoding: utf-8
+
+require 'tempfile'
+
+class CarrierStringIO < StringIO
+  def original_filename
+    "base64.png"
+  end
+
+  def content_type
+    "image/png"
+  end
+end
+
 class Person < ActiveRecord::Base
 
   mount_uploader :portrait, PortraitUploader
@@ -7,5 +21,14 @@ class Person < ActiveRecord::Base
             :division,
             :email,
             :phone_number, presence: true
+
+  def portrait_base64= value
+    super
+
+    binary = Base64.decode64(value).force_encoding('UTF-8').encode
+    io = CarrierStringIO.new binary
+    self.portrait = io
+
+  end
 
 end
--- app/uploaders/portrait_uploader.rb
+++ app/uploaders/portrait_uploader.rb
@@ -5,9 +5,6 @@ class PortraitUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
-
-  process :capture_base64
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -20,19 +17,6 @@ class PortraitUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fill => [64, 64]
-  end
-
-  def original_base64
-    data = model.send "#{mounted_as}_base64"
-    "data:image/#{file.extension};base64,#{data}"
-  end
-
-  def capture_version_base64
-    model.send "#{mounted_as}_#{version_name}_base64=", Base64.encode64(file.read)
-  end
-
-  def capture_base64
-    model.send "#{mounted_as}_base64=", Base64.encode64(file.read)
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
